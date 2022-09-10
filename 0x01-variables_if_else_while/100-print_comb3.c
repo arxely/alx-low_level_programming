4090c63ec08a03dc9301f42ac6962883b9c0abd6
kouam yvan <kouamy87@gmail.com>
2022-09-10 05:01:36 -0700

    100-print_comb3.c to 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 49; i <= 58; i++)
+		for (j = 49; i <= 57; i++)
 		{
 			if (j > i)
 			{

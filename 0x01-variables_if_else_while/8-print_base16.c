30ae0ff84df4ccb5beee5441f6ec4788f64743fe
kouam yvan <kouamy87@gmail.com>
2022-09-10 03:33:10 -0700

    8-print_base16.c added to 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	for(i = 48; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
 	}
-	for(i = 97; i <= 102; i++)
+	for (i = 97; i <= 102; i++)
 	{
 		putchar(i);
 	}
 	putchar(10);
-	
+
 	return (0);
 }

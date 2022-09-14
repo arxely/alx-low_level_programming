543d0f79c607f481b4879fb596097ab9efddeeb6
kouam yvan <kouamy87@gmail.com>
2022-09-14 15:07:40 -0700

    0-putchar.c added to 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for(i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}

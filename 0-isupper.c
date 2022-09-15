150c3eabe7ca7be9c11b5cc1a34eec7883a86e7a
kouam yvan <kouamy87@gmail.com>
2022-09-15 15:01:18 -0700

    0-isupper.c added to 0x04-more_functions_nested_loops

---
+++ 0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *@c: the character to test
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (i <= 65 && i >= 90)
+	{
+		return (1);
+	}
+	return (0);
+}

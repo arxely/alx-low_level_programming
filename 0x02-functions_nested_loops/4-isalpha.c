28174054506535d9996aa651d4c6e7b477d688a1
kouam yvan <kouamy87@gmail.com>
2022-09-14 19:43:42 -0700

    isaalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  *Returns 1 if c is lowercase or uppercase, 0 otherwise
 */
-int _islower(int c)
+int _isalpha(int c)
 {
         if ((c >= 97 && c <= 122) || (c >= 65 && c<= 90))
                 return (1);

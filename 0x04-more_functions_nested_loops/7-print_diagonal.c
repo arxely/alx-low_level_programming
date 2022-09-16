8f8abe9e89b677710aef13ef88db67aafc72d323
kouam yvan <kouamy87@gmail.com>
2022-09-15 18:29:03 -0700

    7-print_diagonal.c added to 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a straight line in the terminal.
+ * @n: number of \ to print
+ * Return: None
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}

ba7ad4f3d68ee2de3869490edf8f66c968de94df
kouam yvan <kouamy87@gmail.com>
2022-09-15 19:12:31 -0700

    7-print_diagonal.c added to 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,19 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\\');
-		_putchar('\n');
+		if (i >= 2)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 	if (n <= 0)
 	{

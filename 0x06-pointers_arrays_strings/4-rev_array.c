2b9b1682b17df40c60f98a703d4965beb8fee8a7
kouam yvan <kouamy87@gmail.com>
2022-09-22 15:55:39 -0700

    4-rev_array.c added to 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,9 +15,8 @@ void reverse_array(int *a, int n)
 
 	for (i = 0, j = n - 1; i < n / 2; i++, j--)
 	{
-		tmp = a[j];
-		a[j] = a[i];
-		a[i] = tmp;
+		tmp = *(a + j);
+		*(a + j) = *(a + i);
+		*(a + i) = tmp;
 	}
-	a = tmp;
 }
